ac82d845ec87fb57576050701f2f7b149ef5d444
Charles Alaras <charles.alaras@gmail.com>
2024-05-20 14:34:48 -0700

    Initial commit

---
+++ src/clf.rs
@@ -0,0 +1,18 @@
+pub fn nearest_neighbor() {
+    let mut dist: f64 = 0;
+    // For each data point compare to the given data point at each feature
+
+    // return a class
+}
+
+pub fn forward_selection() {
+    // Start with no features (default rate)
+}
+
+pub fn backward_elimination() {
+    // Add all features at the start
+
+    // Call K_Fold
+}
+
+pub fn k_fold_cross_validation(k: u32) -> f64 {}
